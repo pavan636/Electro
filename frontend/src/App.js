93fa7eab84360d1e2e4b3b54b2cad1ebca79fdb8
pavantom <pavanbkumar636@gmail.com>
2020-10-27 10:28:30 +0530

    homescreen product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Container>
-          <h1>Welcome TO Electro</h1>
+         <Container>
+        
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
