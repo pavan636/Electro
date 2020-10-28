90a1f35ecc512291350f95eadda14820b5d20cab
pavantom <pavanbkumar636@gmail.com>
2020-10-28 16:54:22 +0530

    Custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
 connectDB()
-
 
 const app = express()
 
@@ -17,6 +16,10 @@ app.get('/', (req, res) =>{
 } )
 
 app.use('/api/products',productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
