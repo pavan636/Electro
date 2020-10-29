218383d0fa62fffddcf5e045fc05bf51a8335095
pavantom <pavanbkumar636@gmail.com>
2020-10-29 17:15:08 +0530

    useDispatch and useSelector

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,26 +1,27 @@
 import axios from 'axios'
-import { 
-    PRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_FAIL, 
-    PRODUCT_LIST_SUCCESS 
-    } from '../constants/productConstants'
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
-    try {
-        dispatch({ type: PRODUCT_LIST_REQUEST})
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await axios.get('/api/products')
+    const { data } = await axios.get('/api/products')
 
-        dispatch({
-            type: PRODUCT_LIST_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.data.message
-            ? error.response.data.message : error.message,
-        })
-
-    }
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
