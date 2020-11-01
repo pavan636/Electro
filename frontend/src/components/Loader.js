19ec9b77f872c214b383b2f0c301496df8038a1e
pavantom <pavanbkumar636@gmail.com>
2020-11-01 17:06:21 +0530

    cart reducer and add to cart action

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -11,7 +11,7 @@ const Loader = () => {
             display: 'block',
            }}
         >
-           <span class='sr-only'>Loading... </span> 
+           <span className='sr-only'>Loading... </span> 
         </Spinner>
     )
 }
