ead8b8ed82fddc9f79d88fa745543f4f9dd1f799
pavantom <pavanbkumar636@gmail.com>
2020-11-01 16:30:09 +0530

    qty select

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CartScreen = () => {
+    return (
+        <div>
+            Cart
+        </div>
+    )
+}
+
+export default CartScreen
+
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools }  from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-
+ 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
