1bad93e879238cf48c81abfb512b3556c9cb09b4
pavantom <pavanbkumar636@gmail.com>
2020-11-08 13:16:08 +0530

    custom authentication user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,6 +26,28 @@ const authUser = asyncHandler(async (req, res) => {
 
 })  
 
+//@desc     Get user Profile 
+//@route    POST /api/users/profile
+//@access   Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+})  
+
+
 export{
-    authUser
+    authUser, getUserProfile
 }
