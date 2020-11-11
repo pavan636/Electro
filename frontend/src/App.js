8a865dbbc937284f2902422ac268cc5f1097cd2c
pavantom <pavanbkumar636@gmail.com>
2020-11-11 15:42:57 +0530

    shipping screen save address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,16 +9,17 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
-
+import ShippingScreen from './screens/ShippingScreen'
 const App = () => {
   return (
     <Router>
     <Header />
     <main className = 'py-3'>
       <Container>
+        <Route path='/shipping' component={ShippingScreen} /> 
         <Route path='/login' component={LoginScreen} />
         <Route path='/register' component={RegisterScreen} />
-         <Route path='/profile' component={ProfileScreen} />
+        <Route path='/profile' component={ProfileScreen} />
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
         <Route path='/' component={HomeScreen} exact />
