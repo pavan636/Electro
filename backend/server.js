5943b0e5ff29e61c7350eb0293632370d3849d06
pavantom <pavanbkumar636@gmail.com>
2020-11-12 18:34:50 +0530

    shipping

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import userRoutes from './routes/userRoutes.js'
-
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 
@@ -21,7 +21,7 @@ app.get('/', (req, res) =>{
 
 app.use('/api/products',productRoutes)
 app.use('/api/users',userRoutes)
-
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 
