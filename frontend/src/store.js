d33ce88f0b99934a0809707a17657dc08db4a022
pavantom <pavanbkumar636@gmail.com>
2020-11-14 15:01:36 +0530

    showing orders on profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,6 +14,7 @@ import {
   orderCreateReducer,
   orderDetailsReducer,
   orderPayReducer,
+  orderListMyReducer,
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -27,6 +28,7 @@ const reducer = combineReducers({
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
+    orderListMy: orderListMyReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
