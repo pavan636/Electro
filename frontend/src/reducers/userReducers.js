e5bbbda69c647a539c951065f9a65646e483733f
pavantom <pavanbkumar636@gmail.com>
2020-11-14 15:46:02 +0530

    admin user

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
@@ -68,6 +71,19 @@ export const userUpdateProfileReducer = (state = {}, action) => {
     case USER_UPDATE_PROFILE_SUCCESS:
       return { loading: false, success: true, userInfo: action.payload }
     case USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const userListReducer = (state = { users: [] }, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { loading: true }
+    case USER_LIST_SUCCESS:
+      return { loading: false, users: action.payload }
+    case USER_LIST_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
