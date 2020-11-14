f5282544aa1fc0b83adfab10dc5b5179d32a7538
pavantom <pavanbkumar636@gmail.com>
2020-11-14 19:44:00 +0530

    Image upload

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools }  from 'redux-devtools-extension'
-import { productListReducer, productCreateReducer, productDetailsReducer, productDeleteReducer } from './reducers/productReducers'
+import { productListReducer,productUpdateReducer, productCreateReducer, productDetailsReducer, productDeleteReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import {
   userLoginReducer,
@@ -25,6 +25,7 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     productDelete: productDeleteReducer,
     productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
