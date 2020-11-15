666f12829b3f2a5748cf4c9050d2d99894633d5e
pavantom <pavanbkumar636@gmail.com>
2020-11-15 22:39:58 +0530

    Search box

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,15 +6,16 @@ import Loader from '../components/Loader'
 import { useDispatch, useSelector } from 'react-redux' 
 import { listProducts } from '../actions/productActions'
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword
     const dispatch = useDispatch()
     
     const productList = useSelector((state) => state.productList)
     const { loading, error, products } = productList 
 
      useEffect(() => {
-    dispatch(listProducts())
-  }, [dispatch])
+    dispatch(listProducts(keyword))
+  }, [dispatch, keyword])
 
   return (
     <>
